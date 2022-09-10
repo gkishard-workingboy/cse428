446118e9abe6fa2495b6e63676e38e3f851ab9f4
jzeng23 <54592831+jzeng23@users.noreply.github.com>
2022-09-09 19:03:04 -0500

    initialize members outside the constructor

--- labs/lab0/Card_T.cpp
+++ labs/lab0/Card_T.cpp
@@ -5,11 +5,7 @@
  *
  */
 template <typename R, typename S>
-Card<R, S>::Card(R r, S s)
-{
-    this->rank = r;
-    this->suit = s;
-}
+Card<R, S>::Card(R r, S s): rank(r), suit(s) {}
 
 template <typename R, typename S>
 std::ostream &operator<<(std::ostream &os, const Card<R, S> &c)
