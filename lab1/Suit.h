5ee8da1c32779892c41a79dc1f9ac7024864b068
yhy <54674135+yehaoyuan96@users.noreply.github.com>
2022-09-28 16:33:35 -0500

    Delete lab1 directory

--- lab1/Suit.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace std;
-#include <ostream>
-
-enum class Suit { clubs, diamonds, hearts, spades, undefined };
-
-std::ostream& operator<<(std::ostream& os, const Suit& s);
-
-Suit& operator++(Suit& s);
